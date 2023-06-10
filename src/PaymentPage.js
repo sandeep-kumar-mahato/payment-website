00a46e62388bd68188ba3f64773f477df40ca006
sandeep kumar mahato <sandeepkumarmahato712@gmail.com>
2023-06-10 11:08:30 +0530

    init

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -1,34 +1,49 @@
+// PaymentPage.js
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { sendInvoiceEmail } from "./emailService"; // Import the email service function
+import emailjs from "emailjs-com";
 
 const PaymentPage = () => {
   const [amount, setAmount] = useState("");
   const [paymentType, setPaymentType] = useState("");
-  const [email, setEmail] = useState(""); // Add state for the user's email
   const navigate = useNavigate();
 
   const handleDonate = () => {
-    // Handle the donation logic and payment gateway integration here
-    // Redirect the user or display a success message
+    const email = prompt("Please enter your email address:");
+    if (email) {
+      // Handle the donation logic and payment gateway integration here
 
-    // Generate the invoice and send it via email
-    const invoice = {
-      amount,
-      paymentType,
-      email,
-      // Include any other relevant information in the invoice
-    };
+      // Send the invoice to the provided email address
+      const templateParams = {
+        to_email: email,
+        subject: "Invoice for Donation",
+        amount: amount, // Add the amount to the template params
+      };
 
-    sendInvoiceEmail(invoice); // Call the email service function to send the invoice
+      // Replace 'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', and 'YOUR_USER_ID' with your actual values
+      emailjs
+        .send(
+          "service_59w9ui6",
+          "template_idexlo9", // Replace this placeholder with your actual EmailJS template ID
+          templateParams,
+          "FM4SOtO9rjvT97whM"
+        )
+        .then(() => {
+          // Success message or further navigation logic
+          console.log("Invoice email sent successfully");
+        })
+        .catch((error) => {
+          // Error handling
+          console.error("Failed to send invoice email:", error);
+        });
 
-    // Clear the form
-    setAmount("");
-    setPaymentType("");
-    setEmail("");
+      // Clear the form
+      setAmount("");
+      setPaymentType("");
 
-    // Example navigation to a success page after donation
-    navigate("/success");
+      // Example navigation to a success page after donation
+      navigate("/");
+    }
   };
 
   return (
@@ -62,18 +77,6 @@ const PaymentPage = () => {
             <option value="paypal">PayPal</option>
             {/* Add other payment types here */}
           </select>
-        </div>
-        <div>
-          <label htmlFor="email" className="block font-medium">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border rounded-md p-2 w-full"
-          />
         </div>
         <button
           type="button"
